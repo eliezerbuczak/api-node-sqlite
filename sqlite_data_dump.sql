d39d1f88e8dc5c4397d303c42e6ebc6509f0c45d
eliezer <eliezerbuczak@gmail.com>
2024-01-07 20:25:54 -0300

    .env e docker-compose para inserção de dados no mysq

--- sqlite_data_dump.sql
+++ sqlite_data_dump.sql
@@ -1,16 +1,3 @@
-PRAGMA foreign_keys=OFF;
-BEGIN TRANSACTION;
-CREATE TABLE usuarios (id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT);
+CREATE TABLE usuarios (id INT PRIMARY KEY AUTO_INCREMENT, nome VARCHAR(255));
 INSERT INTO usuarios VALUES(1,'João');
-INSERT INTO usuarios VALUES(2,'Psss');
-INSERT INTO usuarios VALUES(3,'João');
-INSERT INTO usuarios VALUES(4,'João');
-INSERT INTO usuarios VALUES(5,'João');
-INSERT INTO usuarios VALUES(6,'João');
-INSERT INTO usuarios VALUES(7,'João');
-INSERT INTO usuarios VALUES(8,'João');
-INSERT INTO usuarios VALUES(9,'João');
-INSERT INTO usuarios VALUES(10,'João');
-DELETE FROM sqlite_sequence;
-INSERT INTO sqlite_sequence VALUES('usuarios',10);
-COMMIT;
+INSERT INTO usuarios VALUES(2,'Pedro');
